e6ceb963b2a81a3e6ac2395469636de985b34640
Gideon Etowa <etowagideon@gmail.com>
2024-04-19 00:12:30 +0100

    added blank text field after submitting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="text-center">
-        <p>Patched together with lots of <em class="line-through">love</em> duct tape by <a href=""
+        <p>Patched together with lots of <em class="line-through">love</em> duct tape by <a
+                href="https://gideonetowa.xyz/" target="_blank"
                 class="underline decoration-primary-col text-primary-col">@geed__et</a></p>
         <br><br>
-        <span class="italic">Powered by <a href="">Dictionary API</a>
+        <span class="italic">Powered by <a href="https://dictionaryapi.dev/" target="_blank">Dictionary API</a>
         </span>
     </div>
 </template>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,8 +6,10 @@
                 class="rounded-l-full h-16 w-full px-6 text-primary-txt font-semibold bg-bder-col focus:outline-none focus:ring-2 focus:ring-primary-col" />
             <button class="bg-primary-col w-16 h-[65px] rounded-r-full inline-block p-2"><img
                     src="@/assets/searchicon.svg" alt="" class="w-9 h-9" @click="searchWord"></button>
+
         </div>
     </form>
+    <p>{{ searchedWord }}</p>
 </template>
 
 <script>
@@ -18,23 +20,28 @@ export default {
     data() {
         return {
             search: '',
-            errorMsg: 'Oops!!'
+            errorMsg: 'Oops!!',
+            searchedWord: '',
         }
     },
     methods: {
         submitForm(event) {
-            event.preventDefault()
+            event.preventDefault();
+
         },
 
         searchWord() {
             axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.search}`)
                 .then((response) => {
                     console.log(response.data)
+                    this.searchedWord = response.data
                 })
                 .catch((error) => {
                     console.log(this.errorMsg)
 
                 })
+
+            this.search = ''
         }
     }
 }
