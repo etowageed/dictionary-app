a832e5cb494540b0eca2866fcba19ffe88aa29ad
Gideon Etowa <etowagideon@gmail.com>
2024-10-18 22:43:13 +0100

    fixed toggle switch for light/darkmode

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,28 @@
 import Search from './components/Search.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Toggle from './components/Toggle.vue'
 
 export default {
   name: 'App',
   components: {
-    Search, Header, Footer,
+    Search, Header, Footer, Toggle,
+  },
+
+  provide() {
+    return {
+      toggleBtn: this.toggleDarkBg
+    }
   },
 
   data() {
     return {
-      isDarkBg: 'false' // default theme is white so darkbg is false
+      isDarkBg: false // default theme is white so darkbg is false
 
     };
   },
   methods: {
-    toggleDarkBg() {
+    toggleDarkBg(isDarkBg) {
       this.isDarkBg = !this.isDarkBg;
 
     }
@@ -24,15 +31,14 @@ export default {
 }
 </script>
 
+
 <template>
   <div class="flex flex-col justify-between p-8 text-primary-txt min-h-screen"
     v-bind:class="{ dark: isDarkBg, light: !isDarkBg }">
 
     <Header class=" mb-20" />
 
-    <!-- <Search class="w-3/4 md:w-1/3 mx-auto"></Search> -->
-
-    <button @click="toggleDarkBg">toggle darkmode</button>
+    <Toggle></Toggle>
 
     <Search class="w-2/4 mx-auto" />
 
@@ -44,6 +50,8 @@ export default {
 
 
 </template>
+
+
 
 
 <style>
